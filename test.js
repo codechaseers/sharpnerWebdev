55809518a4be660985b6ba9d066a9de015300a21
sudhanshu shekhar ojha <sudhanshu@gmail.com>
2024-07-06 10:20:31 +0530

    intial commit git add

---
+++ test.js
@@ -0,0 +1,11 @@
+console.log("connect");
+function handleFormSubmit(event) {
+    event.preventDefault(); 
+  console.log("hii");
+// console.log(event.target.Name.value)
+  // Get the form element
+   
+
+  // Log the form values to the console
+  
+}
